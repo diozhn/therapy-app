9b99547ed1947258cd804a4ddb9375b4fd8fc1f5
diozhn <contato.diogomendes@gmail.com>
2021-04-24 13:19:08 -0300

    Upload de atualizações do Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,14 @@
-import React from 'react'
-import Sidebar from './Navigation/Sidebar'
-import Topbar from './Navigation/Topbar'
+import React from "react";
+import Sidebar from "./Navigation/Sidebar";
+import Topbar from "./Navigation/Topbar";
+import PageHeading from "../Dashboard/PageHeading";
+import MonthlyRequests from "../Dashboard/Cards/MonthlyRequests";
+import PendingRequestCard from "../Dashboard/Cards/PendingRequestCard";
+import AnnualRequests from "../Dashboard/Cards/AnnualRequests";
+import AreaChart from "../Dashboard/Graphics/AreaChart";
+import PieChart from "../Dashboard/Graphics/PieChart";
+import ScrollTopButton from "../Dashboard/Details/ScrollTopButton";
+import LogoutModal from "../Dashboard/Details/LogoutModal";
 
 const Dashboard = () => {
   return (
@@ -8,11 +16,27 @@ const Dashboard = () => {
       <div id="wrapper">
         <Sidebar />
         <div id="content-wrapper" className="d-flex flex-column">
-          <Topbar />
+          <div id="content">
+            <Topbar />
+            <div className="container-fluid">
+              <PageHeading />
+              <div className="row">
+                <MonthlyRequests />
+                <PendingRequestCard />
+                <AnnualRequests />
+              </div>
+              <div className="row">
+                <AreaChart />
+                <PieChart />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <ScrollTopButton />
+      <LogoutModal />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/components/Dashboard/Navigation/Topbar.js
+++ src/components/Dashboard/Navigation/Topbar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import imgProfile from "../../../Assets/img/undraw_profile.svg";
 
 const Topbar = () => {
   return (
-    <div id="content">
       <nav className="navbar navbar-content navbar-light bg-white topbar mb-4 static-top shadow">
         <button
           id="sidebarToggleTop"
@@ -63,14 +63,23 @@ const Topbar = () => {
             </div>
           </li>
           <li className="nav-item dropdown no-arrow mx-1">
-            <Link className="nav-link dropdown-toggle" to="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <Link
+              className="nav-link dropdown-toggle"
+              to="#"
+              id="alertsDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
               <i className="fas fa-bell fa-fw"></i>
               <span className="badge badge-danger badge-counter">3+</span>
             </Link>
-            <div className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
-              <h6 className="dropdown-header">
-                Notificações
-              </h6>
+            <div
+              className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+              aria-labelledby="alertsDropdown"
+            >
+              <h6 className="dropdown-header">Notificações</h6>
               <Link className="dropdown-item d-flex align-items-center" to="#">
                 <div className="mr-3">
                   <div className="icon-circle bg-primary">
@@ -79,14 +88,91 @@ const Topbar = () => {
                 </div>
                 <div>
                   <div className="small text-gray-500">Abril 12, 2021</div>
-                  <span className="font-weight-bold">Uma nova consulta foi solicitada!</span>
+                  <span className="font-weight-bold">
+                    Uma nova consulta foi solicitada!
+                  </span>
                 </div>
+              </Link>
+              <Link className="dropdown-item d-flex align-items-center" to="#">
+                <div className="mr-3">
+                  <div className="icon-circle bg-success">
+                    <i className="fas fa-donate text-white"></i>
+                  </div>
+                </div>
+                <div>
+                  <div className="small text-gray-500">24 de Abril, 2021</div>
+                  Você recebeu R$250,00 da pela sua ultima consulta!
+                </div>
+              </Link>
+              <Link className="dropdown-item d-flex align-items-center" to="#">
+                <div className="mr-3">
+                  <div className="icon-circle bg-warning">
+                    <i className="fas fa-exclamation-triangle text-white"></i>
+                  </div>
+                </div>
+                <div>
+                  <div className="small text-gray-500">20 de Abril, 2021</div>
+                  Você recebeu um novo feedback sobre sua ultima consulta!
+                </div>
+              </Link>
+              <Link
+                className="dropdown-item text-center small text-gray-500"
+                to="#"
+              >
+                Mostrar todas as notificações
+              </Link>
+            </div>
+          </li>
+          <div className="topbar-divider d-none d-sm-block"></div>
+          <li className="nav-item dropdown no-arrow">
+            <Link
+              className="nav-link dropdown-toggle"
+              to="#"
+              id="userDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <span className="mr-2 d-none d-lg-inline text-gray-600 small">
+                Diogo
+              </span>
+              <img
+                className="img-profile rounded-circle"
+                src={imgProfile}
+                alt="profile"
+              />
+            </Link>
+            <div
+              className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+              aria-labelledby="userDropdown"
+            >
+              <Link className="dropdown-item" to="#">
+                <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                Perfil
+              </Link>
+              <Link className="dropdown-item" to="#">
+                <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                Configurações
+              </Link>
+              <Link className="dropdown-item" to="#">
+                <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                Atividades Recentes
+              </Link>
+              <div className="dropdown-divider"></div>
+              <Link
+                className="dropdown-item"
+                to="#"
+                data-toggle="modal"
+                data-target="#logoutModal"
+              >
+                <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                Sair
               </Link>
             </div>
           </li>
         </ul>
       </nav>
-    </div>
   );
 };
 
